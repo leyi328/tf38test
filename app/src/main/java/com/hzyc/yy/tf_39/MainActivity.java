d0e5dbb990abeb0d949d195f39b6c534ad442a2f
leyi328 <100828833@qq.com>
2017-10-22 15:29:18 +0800

    首次提交3

---
+++ app/src/main/java/com/hzyc/yy/tf_39/MainActivity.java
@@ -0,0 +1,26 @@
+package com.hzyc.yy.tf_39;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "我爱你中国", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
